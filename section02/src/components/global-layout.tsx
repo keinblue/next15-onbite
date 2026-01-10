86af2c276f3fbd39b59aa041c06ac84d6db9169e
sspark <sspark@sspark.com>
2026-01-10 12:16:58 +0000

    UI 구현

--- section02/src/components/global-layout.tsx
+++ section02/src/components/global-layout.tsx
@@ -2,7 +2,11 @@ import { ReactNode } from "react";
 import Link from "next/link";
 import style from "./global-layout.module.css";
 
-export default function GlobalLayout({ children }: { children: ReactNode }) {
+export default function GlobalLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <div className={style.container}>
       <header className={style.header}>
