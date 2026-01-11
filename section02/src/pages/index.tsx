55c9d59edfa22653e49a2809f050a1cdada5175d
sspark <sspark@sspark.com>
2026-01-11 03:43:14 +0000

    ssg 폴백

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -3,11 +3,12 @@ import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode, useEffect } from "react";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "../lib/fetch-books";
 import fetchRandomBooks from "../lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -24,9 +25,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(allBooks);
-
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   useEffect(() => {
     console.log(window);
   });
